e315a7ebc871e74b314b46aa8aef9820602cb0b7
Priyanshi-Mittal-20 <priyanshimzn05@gmail.com>
2026-06-16 16:43:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your code here
+let arr=[2,1,2]
+function majority(arr)
+{
+	let map=new Map()
+	for(let i of arr)
+		{
+			if(map.has(i))
+			{
+				map.set(i,map.get(i)+1)
+			}
+			else
+			{
+				map.set(i,1)
+			}
+		}
+	for(let [key,value] of map)
+		{
+			if(map.get(key)>Math.floor(arr.length/2))
+			{
+				return key
+			}
+		}
+}
+majority(arr)
